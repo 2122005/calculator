fc74cfbaf3e9689b02c7310e7de6b94a595270c9
2122005 <2122005@mail.ru>
2024-02-14 07:08:19 +0300

    1
    (cherry picked from commit 2b46d6c031dab60e2f6b62025bd324a2bc9a3270)

--- src/Calc.java
+++ src/Calc.java
@@ -77,11 +77,11 @@ public static String parse(String expression) throws Exception {
     static String[] romanArray = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
             "XI", "XII", "XIII", "XIV","XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV",
             "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI",
-            "XXXVII", "XXXVIII", "XXIX", "XL", "XLI","XLII", "XLIII", "XXLIV", "XLV", "XLVI", "XLVII", "XLVIII",
+            "XXXVII", "XXXVIII", "XXIX", "XL", "XLI","XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII",
             "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII","LVIII", "LIX", "LX", "LXI", "LXII", "LXIII",
             "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX","LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI",
             "LXXVII", "LXXVIII", "LXXIX","LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII",
-            "LXXXVIII", "LXXXIX", "XC","C" };
+            "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
 
     public static boolean isRoman(String val) {
         for (int i = 0; i < romanArray.length; i++) {
